8a9897c6bd37a53c00a8708aa27d7a9d8eb07aba
mixalis <mixpri13@gmail.com>
2026-01-14 13:17:01 +0200

    changed the pop up placement in smaller screens

--- src/components/AddProductEffect.jsx
+++ src/components/AddProductEffect.jsx
@@ -5,7 +5,7 @@ import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 export default function AddProductEffect() {
   const addedProducts = useSelector((state) => state.guest.addedEffect);
   return (
-    <div className="fixed z-100 bottom-10 right-20 flex flex-col items-center">
+    <div className="fixed z-100 bottom-10 right-5 md:right-20 flex flex-col items-center">
       {addedProducts.map((item) => (
         <div className={"animateAdd"} key={item.effectId}>
           <p className="text-xs md:text-sm text-wrap">
